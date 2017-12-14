e7ec8b0d04b375b91dec75da70d463b04cc1ed40
Ravi Malik <ravimalik2364@gmail.com>
2017-12-14 16:17:03 -0500

    Web Scraping: Basics, BeautifulSoup Lib
    Learnt basics of web scraping. Contents of web page are fetched
    using requests module and parse tree is built using BeautifulSoup.
    
    Main functions to notice are:
    1. find : find element(s) inside the object in consideration
    2. decompose : remove element(s) from result space
    3. contents : list of children

---
+++ 3_web_scraping/1_basics.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://coinmarketcap.com/"
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+	data = BeautifulSoup(response.text, 'html.parser')
+	crypto_list = data.find("table", {"id": "currencies"})
+
+	# Exclude table header
+	crypto_list.find("thead").decompose()
+
+	crypto_list = crypto_list.find_all("tr")
+
+	# Print crypto names
+	for crypto in crypto_list:
+		name = crypto.contents[3].find("a", {"class": "currency-name-container"})
+		price = crypto.find("a", {"class": "price"})
+
+		print ("{} : {}".format(name.text, price.text))
