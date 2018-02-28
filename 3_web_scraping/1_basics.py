07ab6c7c7820d7eb8e0d77b24c0cea8fa9b91e14
Ravi Malik <ravimalik2364@gmail.com>
2018-02-28 17:27:18 -0500

    User agent header added. For faking browser request.

--- 3_web_scraping/1_basics.py
+++ 3_web_scraping/1_basics.py
@@ -4,8 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://coinmarketcap.com/"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-response = requests.get(URL)
+response = requests.get(URL, headers=headers)
 
 if response.status_code == 200:
 	data = BeautifulSoup(response.text, 'html.parser')
